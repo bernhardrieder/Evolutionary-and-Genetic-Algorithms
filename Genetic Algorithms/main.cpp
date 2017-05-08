61d466671304fb86b3288b513e6f1960b4af1980
Bernhard Rieder <bernhard.rieder@live.at>
2017-05-08 23:42:39 +0200

    added "n queens puzzle" picture creation code

--- Genetic Algorithms/main.cpp
+++ Genetic Algorithms/main.cpp
@@ -1,33 +1,48 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
 
 int main()
 {
-	// create the window
-	sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+	unsigned int queensAmount = 50;
+	unsigned int queensPixel = 20;
+	unsigned int gridPixel = 1;
+	unsigned int pixelAmount = (queensAmount * (queensPixel + gridPixel))- gridPixel; // -gridPixel for the last one where no grid is needed
+	sf::Vector2u textureDimension = { pixelAmount, pixelAmount };
+	sf::RenderTexture renderTexture;
 
-	// run the program as long as the window is open
-	while (window.isOpen())
+	if (!renderTexture.create(textureDimension.x, textureDimension.y))
 	{
-		sf::CircleShape circle(10);
-		// check all the window's events that were triggered since the last iteration of the loop
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			// "close requested" event: we close the window
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		// clear the window with black color
-		window.clear(sf::Color::Black);
-
-		// draw everything here...
-		// window.draw(...);
-		window.draw(circle);
-
-		// end the current frame
-		window.display();
+		std::cerr << "Cannot create rendertexture";
+		getchar();
+		return 0;
 	}
+	renderTexture.clear(sf::Color::White);
+
+	//create grid
+	{
+		sf::VertexArray vertices(sf::PrimitiveType::Lines);
+		for(int x = 0; x < queensAmount; ++x)
+		{
+			int xPos = x*queensPixel + x*gridPixel;
+			sf::Vertex v1(sf::Vector2f(xPos, 0), sf::Color::Black);
+			sf::Vertex v2(sf::Vector2f(xPos, pixelAmount), sf::Color::Black);
+			vertices.append(v1);
+			vertices.append(v2);
+		}
+		for (int y = 0; y < queensAmount; ++y)
+		{
+			int yPos = y*queensPixel + y*gridPixel;
+			sf::Vertex v1(sf::Vector2f(0, yPos), sf::Color::Black);
+			sf::Vertex v2(sf::Vector2f(pixelAmount, yPos), sf::Color::Black);
+			vertices.append(v1);
+			vertices.append(v2);
+		}
+		renderTexture.draw(vertices);
+	}
+	renderTexture.display();
+	if (renderTexture.getTexture().copyToImage().saveToFile(std::to_string(queensAmount) + "_queens_puzzle.jpg"))
+		std::cout << "created image on filesystem";
 
 	return 0;
 }
